cde5425ca2819c819bc144ba5e8eeaf1ecc2b3e5
RishengDeng <dengrisheng1997@gmail.com>
2020-11-12 21:41:14 +0800

    divide npy to four subphase

---
+++ dataprocess/dividephase.py
@@ -0,0 +1,37 @@
+import os 
+import shutil
+import numpy as np 
+
+path = '/media/drs/extra/Datasets/mvi_new/iv_bbox_npy/'
+
+phase_path = []
+
+art_path = path + 'art/'
+nc_path = path + 'nc/'
+pv_path = path + 'pv/'
+dl_path = path + 'dl/'
+
+phase_path.append(art_path)
+phase_path.append(nc_path)
+phase_path.append(pv_path)
+phase_path.append(dl_path)
+
+
+for phase in phase_path:
+    if not os.path.exists(phase):
+        os.makedirs(phase)
+
+
+for item in os.listdir(path):
+    item_path = os.path.join(path, item)
+    if os.path.isdir(item_path):
+        continue
+    if item.split('_')[-2] == 'art':
+        shutil.move(item_path, art_path + item)
+    elif item.split('_')[-2] == 'nc':
+        shutil.move(item_path, nc_path + item)
+    elif item.split('_')[-2] == 'pv':
+        shutil.move(item_path, pv_path + item)
+    elif item.split('_')[-2] == 'dl':
+        shutil.move(item_path, dl_path + item)
+    
